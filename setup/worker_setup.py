8629e1f77ce86cd5e90bd92b1b5c76f1e17b5256
pettersolberg88 <pettersolberg88@gmail.com>
2015-11-25 00:53:15 +0100

    Removed apt-get update to fix "device is busy." error

--- setup/worker_setup.py
+++ setup/worker_setup.py
@@ -208,7 +208,7 @@ def setup_base_chroot(options):
         run_cmd("cp {0}*.deb {1}{0}".format(deb_archives, base_chroot_dir))
         run_cmd("schroot -c aic-base -- /bin/sh -c \"\
                 DEBIANFRONTEND=noninteractive;\
-                apt-get update; apt-get upgrade -y\"")
+                apt-get update\"")
         run_cmd("schroot -c aic-base -- apt-get install -y python")
     run_cmd("schroot -c aic-base -- %s/setup/worker_setup.py --chroot-base"
             % (os.path.join(options.root_dir, options.local_repo),))
