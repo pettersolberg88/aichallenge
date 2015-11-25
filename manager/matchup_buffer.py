06b4b301db54f45474ae9f2c52e3b4b09b2d1b89
pettersolberg88 <pettersolberg88@gmail.com>
2015-11-25 23:43:24 +0100

    Added commit to create matches properly

--- manager/matchup_buffer.py
+++ manager/matchup_buffer.py
@@ -45,7 +45,7 @@ def main():
             for i in range(add):
                 cursor.execute("call generate_matchup")
                 cursor.nextset()
-
+		connection.commit()
 if __name__ == "__main__":
     main()
 
